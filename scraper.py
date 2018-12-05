29c1ffb1cea11d498d8268bb9f67b78f6681b1a7
MrN <sebastianborowski1@gmail.com>
2018-12-05 01:00:52 +0100

    Added cssselect, getResponse function.

--- scraper.py
+++ scraper.py
@@ -1,27 +1,48 @@
 import bs4 as bs
 import requests
 import lxml
+from lxml import html
+import re
+import cssselect
 
 url = "https://en.wikipedia.org/wiki/Regression_testing"
 
 soup = bs.BeautifulSoup("", "lxml")
+bsOutput = str()
+lxmlOutput = str()
 
 def getSourceCode(url, package="bs"):
     if package == "bs":
         global soup
+        global bsOutput
         data = requests.get(url)
         soup = bs.BeautifulSoup(data.content, 'lxml')
+        bsOutput = soup
         return soup.prettify()
     elif package == "lxml":
+        global lxmlOutput
         data = requests.get(url)
         data = data.content
         html = lxml.etree.HTML(data)
-        parsed = lxml.etree.tostring(html, pretty_print=True, method="html")
-        return parsed
+        lxmlOutput = lxml.etree.tostring(html, pretty_print=True, method="html")
+        return lxmlOutput
+
+def getResponse(url):
+        data = requests.get(url)
+        data = data.content
+        html = lxml.etree.HTML(data)
+        return html
         
-    
-print(getSourceCode(url, "lxml"))
 #parsed = soup.findAll("div")
 #print(parsed)
 
+#print(getSourceCode("https://justjoin.it/poznan", "lxml"))
+"""
+data = requests.get(url)
+data = data.content
 
+toFind = re.compile(r'#main-page-portals')
+
+html = lxml.html.HTMLParser(data)
+print(html)
+"""
